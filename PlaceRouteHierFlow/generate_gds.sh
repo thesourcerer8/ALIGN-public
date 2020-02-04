a7964937991ad2dd0e8139e7e1ba731ef354f912
Yaguang <liyg@tamu.edu>
2020-02-03 19:30:55 -0700

    add run.sh

--- PlaceRouteHierFlow/generate_gds.sh
+++ PlaceRouteHierFlow/generate_gds.sh
@@ -23,16 +23,19 @@ mkdir $gds_folder
 
 source /home/yaguang/Desktop/src/work_branch/general/bin/activate
 
-for i in $(seq 0 1 10)
+export LD_LIBRARY_PATH=/usr/local/lib/lpsolve/lp_solve_5.5.2.5_dev_ux64/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/yaguang/Desktop/opt/tensorflow/bazel-bin/tensorflow
+
+for i in $(seq 0 1 1)
 do
-  for j in $(seq 0 1 10)
+  for j in $(seq 0 1 1)
   do
-    for q in $(seq 0 1 10)
+    for q in $(seq 0 1 1)
     do
-      for p in $(seq 0 1 10)
+      for p in $(seq 0 1 1)
       do
 
-       index=$((i*1000+j*100+q*10+p*1)
+       index=$((i*1000+j*100+q*10+p*1))
        mkdir $gds_folder$slash$index
       ./pnr_compiler ./$file_name $lef_file $v_file $map_file layers.json $file_name 1 0 $i $j $q $p | tee log && python json2gds.py $source_folder$slash$source_file $target_file && cp $target_file $gds_folder$slash$index$slash$target_file && cp $Feature_value_file $gds_folder$slash$index$slash$Feature_value_file
 
